42c5dece11ebb84c13933d6de855a76119019898
Delisma <delispence7@gmail.com>
2023-12-05 23:06:38 -0500

    added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {
+  name : "drew",
+  streetAddress : "123 main street "
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+     const newemployee ={
+       ... employee
+     }
+     newemployee[key] = value
+     return newemployee
+}function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}function deleteFromEmployeeByKey(employee, key) {
+  const clonedEmployee = { ...employee };
+  delete clonedEmployee[key];
+  return clonedEmployee;
+}function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
